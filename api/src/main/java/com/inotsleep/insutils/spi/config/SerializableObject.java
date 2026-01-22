ebcf775d0543321826d186f0cfca630d91691021
INotSleep <inotsleepminecraft@gmail.com>
2026-01-22 18:32:02 +0100

    Add before and after serialization and deserialization

--- api/src/main/java/com/inotsleep/insutils/spi/config/SerializableObject.java
+++ api/src/main/java/com/inotsleep/insutils/spi/config/SerializableObject.java
@@ -5,21 +5,26 @@ import org.snakeyaml.engine.v2.nodes.MappingNode;
 public class SerializableObject extends UnsafeSerializableObject {
     @Override
     public void beforeDeserialization(MappingNode node) {
-
+        beforeDeserialization();
     }
 
     @Override
     public void afterDeserialization(MappingNode node) {
-
+        afterDeserialization();
     }
 
     @Override
     public void beforeSerialization(MappingNode node) {
-
+        beforeSerialization();
     }
 
     @Override
     public void afterSerialization(MappingNode node) {
-
+        afterSerialization();
     }
+
+    public void beforeDeserialization() {}
+    public void afterDeserialization() {}
+    public void beforeSerialization() {}
+    public void afterSerialization() {}
 }
